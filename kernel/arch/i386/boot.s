8a64366e232fcd0dd34bcfc3819daeb6f5061601
Inconn <62788103+Inconn@users.noreply.github.com>
2024-05-28 18:13:30 -0500

    paging works now YIPPEE !!!

--- kernel/arch/i386/boot.s
+++ kernel/arch/i386/boot.s
@@ -29,11 +29,59 @@ _boot_page_table1:
 .global _start
 .type _start, @function
 _start:
-	mov $stack_top, %esp
-	and 16, %esp
+	mov $(_boot_page_table1 - 0xC0000000), %edi
+	
+	mov %esi, 0
+1:
+	cmpl $kernel_start, %esi
+	jl 2f
+	cmpl $(kernel_end - 0xC0000000), %esi
+	jge 3f
 
-	push %ebx
-	push %eax
+	movl %esi, %edx
+	orl $0x001, %edx
+	cmp $(text_start - 0xC0000000), %esi
+	jl 6f
+	cmp $(rodata_end - 0xC0000000), %esi
+	jge 6f
+	jmp 5f
+6:
+
+	orl $0x002, %edx
+
+5:
+
+	movl %edx, (%edi)	
+
+2:
+	addl $4096, %esi
+	addl $4, %edi
+	loop 1b
+3:
+	movl $(0x000B8000 | 0x003), _boot_page_table1 - 0xC0000000 + 1023 * 4
+
+	movl $(_boot_page_table1 - 0xC0000000 + 0x003), _boot_page_directory - 0xC0000000 + 0
+	movl $(_boot_page_table1 - 0xC0000000 + 0x003), _boot_page_directory - 0xC0000000 + 768 * 4
+
+	movl $(_boot_page_directory - 0xC0000000), %ecx
+	movl %ecx, %cr3
+
+	movl %cr0, %ecx
+	orl $0x80010001, %ecx
+	movl %ecx, %cr0
+
+	lea 4f, %ecx
+	jmp *%ecx
+
+.section .text
+
+4:
+	movl $0, _boot_page_directory + 0
+
+	movl %cr3, %ecx
+	movl %ecx, %cr3
+
+	mov $stack_top, %esp
 
 	call _init
 
--- kernel/kernel/kernel.c
+++ kernel/kernel/kernel.c
@@ -10,22 +10,22 @@ extern uint32_t kernel_end;
 void kernel_main(uint32_t mboot_magic, void* mboot_header) {
 	terminal_initialize();
 
-	struct buddy_allocator* b = (struct buddy_allocator*)kernel_end;
+	//struct buddy_allocator* b = (struct buddy_allocator*)kernel_end;
 
 	void* data = (void*)(kernel_end + (4096 - (kernel_end % 4096)));
 
-	if (buddy_initialize_allocator(b, data, 1073741824, 4096) != 0) {
+/*	if (buddy_initialize_allocator(b, data, 1073741824, 4096) != 0) {
 		terminal_writestring("couldn't init buddy allocator\n");
 		abort();
-	}
+	}*/
 	
 	arch_init_kernel();
 	terminal_writestring("welcome to kitsuneOS!\nthis is all it does right now...\n");
 
-	char* allocated = (char*)buddy_allocate(b, 40);
+	//char* allocated = (char*)buddy_allocate(b, 40);
 
-	allocated = "this is a buddy allocated string\n\0";
+	//allocated = "this is a buddy allocated string\n\0";
 
-	terminal_writestring(allocated);
+	//terminal_writestring(allocated);
 	terminal_writestring("wow!\n");
 }
